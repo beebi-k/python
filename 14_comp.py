93e5d1babf33e2c7534f36e85042c58eb40a32db
beebi-b <kalagotlabeebi95@gmail.com>
2026-06-30 18:32:57 +0530

    comprehensions

---
+++ 14_comp.py
@@ -0,0 +1,12 @@
+# separate vowels and consonants in string "hi hello raju"
+
+s = "hi hello raju"
+
+v = [val for val in s if val in "aeiou"]
+c = [val for val in s if val != " " and val not in "aeiou"]
+
+print(v)
+print(c)
+vow =[]
+con=[]
+l=[vow.append(i) if i in ['a','e','i','o','u'] else con.append(i) for i in s if i!=' ']
