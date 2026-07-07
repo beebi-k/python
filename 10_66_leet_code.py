0ecc60b98ec675a54bc42a2fa71e5b8fcc2fe99c
beebi-b <kalagotlabeebi95@gmail.com>
2026-07-07 18:29:50 +0530

    leet code problem 66

--- 10_66_leet_code.py
+++ 10_66_leet_code.py
@@ -1,27 +1,12 @@
-from typing import List
+digits = [0,9]
 
+for i in range(len(digits) - 1, -1, -1):
+    if digits[i] < 9:
+        digits[i] += 1
+        break
+    else:
+        digits[i] = 0
+else:
+    digits = [1] + digits
 
-class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-
-        for i in range(len(digits)-1,-1,-1):
-
-            if digits[i] < 9:
-                digits[i] += 1
-                return digits
-
-            digits[i] = 0
-
-        return [1] + digits
-
-
-
-# Test code
-
-solution = Solution()
-
-digits = [9,9]
-
-result = solution.plusOne(digits)
-
-print(result)
+print(digits)
